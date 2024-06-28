568f887f5ee4936ca36dd6879e0066313de2fe86
GabrielAntunes34 <antunes.gabriel.345@gmail.com>
2024-06-28 14:34:00 -0300

    Items' logic created and death screen
    Co-authored-by: PedroLFigueiredo <PedroLFigueiredo@users.noreply.github.com>
    Co-authored-by: Guilherme Torquato <trqt@users.noreply.github.com>
    Co-authored-by: Shogo Shima <shogoshima@users.noreply.github.com>

--- rogue.asm
+++ rogue.asm
@@ -1,16 +1,32 @@
 ; Adaptação do Rogue The Videogame, para a arquitetura do ICMC
 jmp main
 
-memoryMap: var #1200		; Mapa da tela na memória, para facilitar o cálculo das posições
-room: var #6                ; room: 0: position, 1: heigth, 2: width, 3: nrmDoors, 4: nrmEnemies, 5: ptrItem, 
+memoryMap: var #1200 		; Mapa da tela na memória, para facilitar o cálculo das posições
+room: var #6                ; room: 0: position, 1: height, 2: width, 3: nmrDoors, 4: nrmEnemies, 5: ptrItem, 
 Doors: var #4			    ; Doors: 0: upperDoor, 1: leftDoor, 2: lowerDoor, 3: rigthDoor
-
+							; Código pro item: 0: x, 1: m, 2: d, 3: v
 ; Sprites da sala
 roomSprites: var #4
 static roomSprites + #0, #'|'
 static roomSprites + #1, #'/'
 static roomSprites + #2, #'-'
 static roomSprites + #3, #'.'
+
+healthPotion: var #2
+static healthPotion + #0, #'x'
+static healthPotion + #1, #15
+
+CoinPotion: var #2
+static CoinPotion + #0, #'m'
+static CoinPotion + #1, #100
+
+DamagePotion: var #2
+static DamagePotion + #0, #'d'
+static DamagePotion + #1, #10
+
+MaxHealthPotion: var #2
+static MaxHealthPotion + #0, #'v'
+static MaxHealthPotion + #1, #15
 
 player: var #5				; player: 0: posPlayer, 1: life, 2: maxLife, 3: coins, 4: damage
 spritePlayer: var #1  
@@ -23,6 +39,7 @@ UI1: string "$$ : \0"
 
 Msn0: string "V O C E   M O R R E U !!!"
 Msn1: string "Quer jogar novamente? <s/n>"
+
 
 ; Tabela de valores randômicos... Ainda falta arrumar algumas coisas
 IncRand: var #1			; Incremento para circular na Tabela de nr. Randomicos
@@ -109,16 +126,22 @@ mainTittleLoop:
 	loadn r5, #IncRand
 	storei r5, r4			; salvando em incRand a seed
 
+mainRestart:
+	; Gerando o item do level atual
+	call GenerateRand
+	loadn r5, #4
+	mod r2, r2, r5
 	call PlayerSetUp
 
 mainNewLevel:
+	; Setando o mapa
     call MapSetUp
 	call UISetUp
+	call SpawnItem
 
 mainGameLoop:
-    inchar r0               ; r0 recebe o comando do jogador, garantindo que nada ocorra até o input
-    
 	; Esperando o turno do jogador
+    inchar r0               ; r0 recebe o comando do jogador
 	loadn r1, #255
 	cmp r0, r1
 	jeq mainGameLoop
@@ -126,9 +149,9 @@ mainGameLoop:
 	; caso o jogador saia do jogo
     loadn r1, #'q'
     cmp r0, r1
-    jeq mainEnd
+    jeq mainDeathSecren
 
-    ; Processando as mudanças para o jogador
+    ; Processando os turnos do jogador e dos inimigos
     call ProcessPlayer
 	;call ProcessEnemy
 
@@ -140,6 +163,16 @@ mainGameLoop:
 	cmp r0, r1
 	jeq mainDeathSecren
 
+	; Caso não tenha mais inimigos na sala --> Colocar isso em processEnemies??
+	;loadn r0, #room
+	;loadn r2, #4
+	;add r0, r0, r2
+	;loadi r0, r0			; r0 recebe nmrEnemies
+	;cmp r0, r1
+	;jne mainProcessDoor
+	;call SpawnItem			; se nmrEnemies == 0
+
+mainProcessDoor:
     jmp mainGameLoop
 
 mainEnd:
@@ -148,11 +181,21 @@ mainEnd:
 
 mainDeathSecren:
 	call RefreshScreen
-	loadn r0, #0
-	loadn r1, #Msn0
-	loadn r2, #0
-	call PrintString
-	jmp mainEnd				; Falta um loop aqui...
+	loadn r1, #tela1Linha0
+	call PrintScreen
+
+	loadn r1, #'y'
+	loadn r2, #'n'
+	loadn r3, #255
+mainDeathScreenLoop:
+	inchar r0				; Esperando uma ação do usuário 
+	cmp r0, r3
+	jeq mainDeathScreenLoop ; Espere caso ele não clique em nada
+	cmp r0, r2
+	jeq main				; Se n, volte a tela  de título
+	cmp r0, r1
+	jeq mainRestart			; se y, recomece
+	jmp mainDeathScreenLoop	; se qualquer outra coisa, espere
 
 ; GenerateRand(): Retorna em r2 o número aleatório
 GenerateRand:
@@ -243,7 +286,7 @@ UISetUp:
 
 
 
-;PlayerSetUp
+;PlayerSetUp()
 ; Inicializa as variáveis do jogador quando começamos o jogo
 PlayerSetUp:
 	push r0
@@ -278,9 +321,9 @@ PlayerSetUp:
 ProcessPlayer:
     push r1
     push r2
-    load r1, player      ; Posição do player na tela
+    load r1, player      	; Posição do player na tela
     load r2, spritePlayer   ; Carregando a posição do player em r2
-    load r5, player      ; Salvando em r5 para pintar de preto
+    load r5, player      	; Salvando em r5 para pintar de preto
 
     call MovPlayer
     cmp r1, r5
@@ -367,26 +410,177 @@ MovPlayerS:
 HandleWallColision:
 	push r0
 	push r2
+	push r4
+	push r5
 
 	loadn r0, #'-'			; Carregando '-' em r0 para comparar com o mapa
 	loadn r2, #memoryMap	
 	add r2, r2, r3
-	loadi r2, r2			; r4 recebe o que há no mapa na posição r3
+	loadi r2, r2			; r2 recebe o que há no mapa na posição r3
 	cmp r2, r0
 	jeq HandleWallColisionEnd ; se novaPosição == '-', não mudaremos a posição antiga (r1)
 	mov r1, r3				; senão, r1 = novaPosição (r3)
 
+	; Com a posição atualizada, verificamos se há item
+	loadn r0, #'x'
+	cmp r2, r0
+	jeq Grabhealthpotion
+
+	loadn r0, #'m'
+	cmp r2, r0
+	jeq Grabcoinpotion
+
+	loadn r0, #'d'
+	cmp r2, r0
+	jeq Grabdamagepotion
+
+	loadn r0, #'v'
+	cmp r2, r0
+	jeq Grabmaxhealthpotion
+
 HandleWallColisionEnd:
+	; Verificando se um item foi obtido
+	loadn r0, #1
+	cmp r2, r0			; Resetando, se r0 == 1
+	jne HandleWallColisionExit
+
+	; Tirando item do mapa da memória e da tela
+	loadn r4, #memoryMap
+	add r4, r4, r1
+	loadn r5, #0
+	storei r4, r5		; colocando '' para onde havia o item
+	
+	call OpenDoors
+	call UISetUp
+
+HandleWallColisionExit:
+	pop r5
+	pop r4
 	pop r2
 	pop r0
 	rts 
 
+Grabhealthpotion:
+	push r1
+	push r3
+	push r4
+	push r5
+
+	loadn r1, #player
+	inc r1				; r1 recebe o endereço de life
+	loadi r3, r1 		; r3 recebe o valor nesse endereço                 
+	
+	loadn r2, #2
+	add r5, r1, r2		; passando enderenço de maxLife
+	loadi r5, r1		; r5 recebe o valor de maxLife
+
+	loadn r4, #healthPotion
+	inc r4
+	loadi r4, r4		; r4 recebe o valor da healthPotion
+	add r3, r4, r3		; r3 = r3 + r4 (valor somado: health temporario)
+
+	cmp r3, r5			; caso r3 seja menor que r5(maxLife), pular
+	jel skipHealthToMaxHealth
+	mov r3, r5
+
+skipHealthToMaxHealth:	
+	storei r1, r3
+
+	loadn r2, #1		; Indicará que as portas deverão abrir
+
+	pop r5
+	pop r4
+	pop r3
+	pop r1
+	jmp HandleWallColisionEnd
 
 
-; MapSetUp(r1 = posPlayer), cria um novo nível
+Grabcoinpotion:
+	push r1
+	push r3
+	push r4
+
+	loadn r1, #player
+	loadn r2, #3     
+	add r1, r1, r2			; r1 recebe o endereço de coin
+	loadi r3, r1            ; r3 recebe o valor nesse endereço
+	loadn r4, #CoinPotion
+	inc r4
+	loadi r4, r4			; r5 recebe o valor de CoinPotion
+	add r3, r4, r3			; r3 = r3 + r5			
+	storei r1, r3
+
+	loadn r2, #1		; Indicará que as portas deverão abrir
+
+	pop r4
+	pop r3
+	pop r1
+	jmp HandleWallColisionEnd
+
+Grabdamagepotion:
+	push r1
+	push r3
+	push r4
+
+	loadn r1, #player
+	loadn r2, #4     
+	add r1, r1, r2			; r1 recebe o endereço de damage
+	loadi r3, r1            ; r3 recebe o valor nesse endereço
+	loadn r4, #DamagePotion
+	inc r4
+	loadi r4, r4
+	add r3, r4, r3
+	storei r1, r3
+
+	loadn r2, #1		; Indicará que as portas deverão abrir
+
+	pop r4
+	pop r3
+	pop r1
+	jmp HandleWallColisionEnd
+
+Grabmaxhealthpotion:
+	push r1
+	push r3
+	push r4
+
+	loadn r1, #player
+	loadn r2, #2     
+	add r1, r1, r2			; r1 recebe o endereço de maxHealth
+	loadi r3, r1            ; r3 recebe o valor nesse endereçø
+	loadn r4, #MaxHealthPotion
+	inc r4
+	loadi r4, r4
+	add r3, r4, r3
+	storei r1, r3
+
+	loadn r2, #1		; Indicará que as portas deverão abrir
+
+	pop r4
+	pop r3
+	pop r1
+	jmp HandleWallColisionEnd
+
+SetEnemies:
+	push r0
+	push r1
+
+	loadn r0, #4
+	loadn r1, #room
+	add r1, r1, r0
+	
+	loadn r0, #0
+	storei r1, r0
+
+	pop r1
+	pop r0
+	rts
+
+
+; MapSetUp(r2 = item), cria um novo nível
 MapSetUp:
 	push r0				    ; r0 = position
-	push r2					; r2 = heigth
+	push r2					; r2 = height
 	push r3					; r3 = width
 	push r4					; r4 = nrmDoors
 	push r5					; r5 = item
@@ -395,11 +589,12 @@ MapSetUp:
 
     call RefreshScreen
 	call RefreshMemoryMap
+	call SetItemInRoom
     call CreateRoom
 	call DrawnRoom
 	call SetMemoryMap
 	call SetPlayerInRoom
-	;call SetEnemies
+	call SetEnemies
 
 	pop r1
 	pop r6
@@ -459,6 +654,20 @@ RefreshMemoryMapLoop:
     rts
 
 
+;SetItemInRoom(r2 = item); Guarda no vetor da sala um item
+SetItemInRoom:
+	push r1
+	push r3
+
+	loadn r1, #room
+	loadn r3, #5
+	add r1, r1, r3			; r1 recebe o endereço para guardar o item na sala
+
+	storei r1, r2 			; Guardando na sala e retornando
+	pop r3
+	pop r1
+	rts
+
 
 ; CreateRoom(r1 = posPlayer);
 ; Slva na memória a sala gerada aleatoriamente, atualizando a posição inicial do player
@@ -468,10 +677,8 @@ CreateRoom:
 
 	; Nova posição
 	loadn r1, #room
-	;call GenerateRand		; r2 recebe um aleatório
-	loadn r0, #166				
-	loadn r2, #4
-	storei r1, r2
+	loadn r0, #166			; Supostamente calculariamos em r0 a pos mais ao meio	
+	storei r1, r0
 
 	; Nova altura
 	inc r1
@@ -490,8 +697,6 @@ CreateRoom:
 	call CreateNewDoors		; Retorna nrmdoors em r4
 	storei r1, r4
 
-	; Falta gerar novos itens...
-
 	mov r2, r6				; Restaurando a altura em r2
 	rts
 
@@ -503,7 +708,7 @@ CreateNewDoors:
 	push r1
 	push r2
 	push r3
-	push r4
+	push r5
 
 	; Gerando o número de portas aleatório entre 1 e 3
 	loadn r3, #3
@@ -512,21 +717,22 @@ CreateNewDoors:
 	inc r1					; Garante que r1 estará entre 1 e 3
 
 	; Instanciando as novas portas
-	loadn r4, #Doors			; r4 armazena o endereço do vetor das portas
+	inc r3					; r3 recebe 4 para gerar, com mod, os códigos dos itens
+	loadn r4, #Doors		; r4 armazena o endereço do vetor das portas
 	loadn r5, #0			; Contador do loop
 
 CreateNewDoorsLoop:
 	call GenerateRand
-	mod r2, r2, r3		    ; Gerando o valor que indica o que terá na porta (de 1 a 3)
-	inc r2
+	mod r2, r2, r3		    ; Gerando o valor que indica o que terá na porta (de 0 a 3)
 	storei r4, r2		    ; Guardando em Doors
 	inc r4					; Doors++
 	inc r5					; Contador++
-	cmp r1, r5
-	jle CreateNewDoorsLoop	; jel = jump equal or lesses (<=)
+	cmp r5, r1
+	jel CreateNewDoorsLoop	; jel = jump equal or lesser (<=)
 
-	; Resetando as portas remanescentes (para 0), caso o número gerado seja menor que 4
-	loadn r2, #0
+	; Resetando as portas remanescentes (para 10), caso o número gerado seja menor que 4
+	dec r3
+	loadn r2, #10
 	cmp r5, r3
 	jel CreateNewDoorsReset	; jle = jump lesser (<)
 
@@ -534,7 +740,7 @@ CreateNewDoorsLoop:
 CreateNewDoorsEnd:
 	mov r4, r1				; Salvando nrmDoors no retorno
 
-	pop r4
+	pop r5
 	pop r3
 	pop r2
 	pop r1
@@ -545,7 +751,7 @@ CreateNewDoorsReset:
 	inc r4
 	inc r5					; contador++
 	cmp r5, r3
-	jel CreateNewDoorsReset ; Itera enquanto contador < 3
+	jle CreateNewDoorsReset ; Itera enquanto contador < 3
 	jmp CreateNewDoorsEnd
 
 
@@ -579,10 +785,10 @@ SetMemoryMapTop:
 	loadn r6, #1			; Resetando o contador para multiplicar o número de linhas
 SetMemoryMapLoop:
 	storei r1, r7
-	add r1, r1, r3				; r1 = r1 + (width + 1)
+	add r1, r1, r3			; r1 = r1 + (width + 1)
 	storei r1, r7
 
-	inc r6						; Calculando a posição da próxima linha
+	inc r6					; Calculando a posição da próxima linha
 	loadn r1, #40
 	mul r1, r1, r6
 	add r1, r1, r0
@@ -609,7 +815,7 @@ SetMemoryMapBottom:
 
 
 
-; DrawRoom(r0 = pos, r2 =  heigth, r3 = width)
+; DrawRoom(r0 = pos, r2 =  height, r3 = width)
 DrawnRoom:
 	push r1					; Auxiliar para r0
 	push r2					; Será usado para calcular a altura máxima
@@ -730,6 +936,190 @@ SetPlayerInRoom:
 	rts
 
 
+; SpawnItem(): Imprime o item na tela e permite a colisão
+SpawnItem:
+	push r0
+	push r1
+	push r2
+	push r3
+	push r4
+	push r5
+
+	; Guardando em r0 o código do item
+	loadn r0, #room
+	loadn r1, #5
+	add r0, r0, r1
+	loadi r0, r0			; r0 recebe o código do item
+
+	; Gerando em r2, uma posição aleatória dentro da sala
+	loadn r5, #2
+	loadn r1, #room
+	inc r1
+	loadi r3, r1	
+	div r3, r3, r5			; r3 recebe medade de height
+	inc r3
+	inc r1
+	loadi r4, r1
+	div r4, r4, r5			; r4 recebe metade do width
+	inc r4
+	load r2, room			; r2 recebe pos
+
+	add r2, r2, r4
+	loadn r5, #40
+	mul r3, r3, r5			; tratando r3 para descer r2 na sala
+	add r2, r2, r3			; r2 se encontra mais ou menos ao centro da sala
+	
+	; Carregando o sprite do item exato a partir do código em r3
+	loadn r3, #0
+	cmp r0, r3
+	jeq SpawnItemHealth
+	loadn r3, #1
+	cmp r0, r3
+	jeq SpawnItemCoin
+	loadn r3, #2
+	cmp r0, r3
+	jeq SpawnItemDamage
+	loadn r3, #3
+	cmp r0, r3
+	jeq SpawnItemMax
+
+SpawnItemEnd:
+	; Carregando a posição do item no memoryMap
+	loadn r1, #memoryMap
+	add r1, r1, r2
+	storei r1, r3
+	; Imprimindo o sprite na tela
+	outchar r3, r2
+
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
+
+SpawnItemHealth:
+	load r3, healthPotion 
+	jmp SpawnItemEnd
+SpawnItemCoin:
+	load r3, CoinPotion
+	jmp SpawnItemEnd
+SpawnItemDamage:
+	load r3, DamagePotion
+	jmp SpawnItemEnd
+SpawnItemMax:
+	load r3, MaxHealthPotion
+	jmp SpawnItemEnd
+
+
+; OpenDoors(): Imprime na tela e no memoryMap as portas selecionadas
+OpenDoors:
+	push r0					; ponteiro para as variáveis
+	push r1					; pos
+	push r2					; Auxiliar para os cálculos
+	push r3					; width
+	push r4					; Auxiliar para os cálculos
+	push r5					; recebe o código de cada porta
+	push r6					; auxiliar para verificar se há uma porta
+	push r7					; Indica qual a posição da porta
+
+	; Carregando os atributos da sala para o cálculo das posições
+	loadn r0, #room
+	loadn r4, #2
+	loadn r2, #40
+	loadi r1, r0			; r1 recebe pos
+	add r1, r1, r2
+
+	add r0, r0, r4
+	loadi r3, r0			; r3 recebe width
+	inc r3					; Ajustando para a ultima parede
+
+	; Calculando a primeira posição
+	mov r7, r1
+	add r7, r7, r3
+	loadn r4, #80			; r4 recebe 80 para descer 2 posições nas próximas portas
+
+	; Verificando se existem e carregando as portas
+	loadn r0, #Doors 
+	loadn r6, #10
+OpenDoorsLoop:
+	loadi r5, r0
+	inc r0
+	cmp r5, r6
+	jeq OpenDoorsEnd
+	call DrawnDoor
+	add r7, r7, r4
+	jmp OpenDoorsLoop
+	
+OpenDoorsEnd:
+	pop r7
+	pop r6
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
+
+
+; DrawnDoor(r5 = código da porta, r7 = posPorta)
+DrawnDoor:
+	push r0					; ponteiro para memoryMap
+	push r1					; Recebe o código da cor da porta
+	push r2					; Recebe o sprite da porta '/'
+	push r6					; Auxiliar para saber o que há na porta
+
+	; Salvando a porta no mapa da memória
+	loadn r0, #memoryMap
+	loadn r2, #'/'
+	add r0, r0, r7
+	storei r0, r2
+
+	; Verificando qual a cor da porta
+	loadn r6, #0
+	cmp r5, r6
+	jeq DrawnDoorGreen
+	loadn r6, #1
+	cmp r5, r6
+	jeq DrawnDoorYellow
+	loadn r6, #2
+	cmp r5, r6
+	jeq DrawnDoorBlue
+	loadn r6, #3
+	cmp r5, r6
+	jeq DrawnDoorRed
+
+DrawnDoorEnd:
+
+	pop r6
+	pop r2
+	pop r1
+	pop r0
+	rts
+
+DrawnDoorGreen:
+	loadn r1, #512
+	add r2, r2, r1
+	outchar r2, r7
+	jmp DrawnDoorEnd
+DrawnDoorYellow:
+	loadn r1, #2816
+	add r2, r2, r1
+	outchar r2, r7
+	jmp DrawnDoorEnd
+DrawnDoorBlue:
+	loadn r1, #3072
+	add r2, r2, r1
+	outchar r2, r7
+	jmp DrawnDoorEnd
+DrawnDoorRed:
+	loadn r1, #256
+	add r2, r2, r1
+	outchar r2, r7
+	jmp DrawnDoorEnd
+
 ;********************************************************
 ;               IMPRIME TELA
 ;********************************************************	
@@ -742,21 +1132,26 @@ PrintScreen: 	;  Rotina de Impresao de Cenario na Tela Inteira
 	push r2	; protege o r1 na pilha para preservar seu valor
 	push r3	; protege o r3 na pilha para ser usado na subrotina
 	push r4	; protege o r4 na pilha para ser usado na subrotina
-	push r5	; protege o r4 na pilha para ser usado na subrotina
+	push r5	; protege o r5 na pilha para ser usado na subrotina
+	push r6	; protege o r6 na pilha para ser usado na subrotina
 
 	loadn R0, #0  	; posicao inicial tem que ser o comeco da tela!
+	loadn R2, #256	; Cor vermelha
 	loadn R3, #40  	; Incremento da posicao da tela!
 	loadn R4, #41  	; incremento do ponteiro das linhas da tela
 	loadn R5, #1200 ; Limite da tela!
+	loadn R6, #tela1Linha0	; Endereco onde comeca a primeira linha do cenario!!
 	
    PrintScreen_Loop:
-		call PrintString
+		call PrintString2
 		add r0, r0, r3  	; incrementaposicao para a segunda linha na tela -->  r0 = R0 + 40
 		add r1, r1, r4  	; incrementa o ponteiro para o comeco da proxima linha na memoria (40 + 1 porcausa do /0 !!) --> r1 = r1 + 41
+		add r6, r6, r4  	; incrementa o ponteiro para o comeco da proxima linha na memoria (40 + 1 porcausa do /0 !!) --> r1 = r1 + 41
 		cmp r0, r5			; Compara r0 com 1200
 		jne PrintScreen_Loop	; Enquanto r0 < 1200
 
-	pop r5	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
+	pop r6	; Resgata os valores dos registradores utilizados na Subrotina da Pilha
+	pop r5
 	pop r4
 	pop r3
 	pop r2
@@ -954,31 +1349,31 @@ tela0Linha29 : string "                                        "
 ; Declara e preenche tela linha por linha (40 caracteres):
 tela1Linha0  : string "                                        "
 tela1Linha1  : string "                                        "
-tela1Linha2  : string "                                        "
-tela1Linha3  : string "                                        "
-tela1Linha4  : string "                                        "
-tela1Linha5  : string "                                        "
-tela1Linha6  : string "                                        "
-tela1Linha7  : string "                                        "
-tela1Linha8  : string "                                        "
-tela1Linha9  : string "                                        "
+tela1Linha2  : string "              VOCE MORREU               "
+tela1Linha3  : string "              VOCE MORREU               "
+tela1Linha4  : string "              VOCE MORREU               "
+tela1Linha5  : string "              VOCE MORREU               "
+tela1Linha6  : string "              VOCE MORREU               "
+tela1Linha7  : string "              VOCE MORREU               "
+tela1Linha8  : string "              VOCE MORREU               "
+tela1Linha9  : string "              VOCE MORREU               "
 tela1Linha10 : string "                                        "
-tela1Linha11 : string "                                        "
-tela1Linha12 : string "                                        "
-tela1Linha13 : string "                                        "
-tela1Linha14 : string "                                        "
-tela1Linha15 : string "                                        "
-tela1Linha16 : string "                                        "
-tela1Linha17 : string "                                        "
-tela1Linha18 : string "                                        "
-tela1Linha19 : string "                                        "
-tela1Linha20 : string "                                        "
-tela1Linha21 : string "                                        "
-tela1Linha22 : string "                                        "
-tela1Linha23 : string "                                        "
+tela1Linha11 : string "                  ..==.                 "
+tela1Linha12 : string "                    .==#%%=.            "
+tela1Linha13 : string "          ...:-+=.......:#**##....      "
+tela1Linha14 : string "==.. .::.:+*+++*%%@%%%%%%#%@@@@@@@#---.."
+tela1Linha15 : string ".++*=++*##*%%*@@%%%##+*++*#%@%##%%@#+*#="
+tela1Linha16 : string "  .#%**=::..-#%%#@@%+-:.--=+%%%%%++#@@*."
+tela1Linha17 : string "    ..:=+#*+*=-==+#**+++*#%*@@@@%#+#... "
+tela1Linha18 : string "*%%#*%@@@%@%%%@**@%%%%%@#-. ..#%#%=...  "
+tela1Linha19 : string "=###=..                     ..*%#%.     "
+tela1Linha20 : string "                           :#*++.       "
+tela1Linha21 : string "                          .-*#%+.       "
+tela1Linha22 : string "                          .***.         "
+tela1Linha23 : string "                            .           "
 tela1Linha24 : string "                                        "
 tela1Linha25 : string "                                        "
-tela1Linha26 : string "                                        "
-tela1Linha27 : string "                                        "
+tela1Linha26 : string "       PRESSIONE Y PARA RESPAWNAR       "
+tela1Linha27 : string "       OU N PARA VOLTAR AO INICIO       "
 tela1Linha28 : string "                                        "
 tela1Linha29 : string "                                        "
